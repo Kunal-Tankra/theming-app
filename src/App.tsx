dd1cdd77f2bd1150a7bd47e858a005f48c05eb2b
Kunal Tankra <kunaltankra2002@gmail.com>
2025-07-31 17:28:48 +0530

    created project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="text-blue-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque illum commodi voluptatem exercitationem quisquam magnam nulla harum non, ad eligendi suscipit iure deleniti iste. Ex maiores itaque qui non.</div>;
+}
+
+export default App;
